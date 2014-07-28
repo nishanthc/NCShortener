883e0866c5180bf9411cc936104d2353ff8287fd
Nishanth Chandradas <nishanthchandradas@gmail.com>
2014-07-28 23:04:54 +0100

    Renamed application and removed extra folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140727024253) do
+
+  create_table "url_tables", force: true do |t|
+  end
 
 end
