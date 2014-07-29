4cbfe5afc2b0892be98a0fa2f264ca8872705da1
Nishanth Chandradas <nishanthchandradas@gmail.com>
2014-07-29 04:37:12 +0100

    Need to get "long_url" variable to read in controller so it can be passed to the main page.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
   def index
+    @short_code = params[:short_code]
+    @long_url = params[:long_url]
     @urls = Url.new
   end
 end
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,16 +5,19 @@ class UrlsController < ApplicationController
 
 
   def create
+
     # Instantiate a new object using form parameters
     @url = Url.new(params.require(:url).permit(:long_url))
+    @long_url = params[:long_url]
     @url.ip_address = request.remote_ip
     # Save the object
     if @url.save
       # If save succeeds, redirect to the index action
-      redirect_to(:action => 'index')
+      redirect_to root_url(:short_code => "sgs",:long_url => @long_url), :notice => 'Your short URL has been created!'
     else
       # If save fails, redisplay the form so user can fix problems
-      render('index')
+redirect_to root_url, notice: "You must enter a URL to be shortened."
+
     end
   end
 end
